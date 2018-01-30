57ab81da3230b64db1e92da8d330859296c0d003
Himanshu Pandey <phiman@INphiman.local>
2018-01-30 09:47:54 +0530

    Header, homepage and about us page added2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-/* eslint-disable no-console */
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import AboutHomePage from "./components/about/AboutHomePage";
+import HomePage from "./components/home/HomePage";
+import App from "./components/App";
 
-console.log("hello");
+// render (
+//     <Router history={browserHistory}
+//         route={route}
+//     />,
+//     document.getElementById('mainApp')
+// );
+render(
+    <Router history={browserHistory} >
+      <div>
+        <Route path="/" component={App}>
+            <IndexRoute component={HomePage}/>
+            <Route path="about" component={AboutHomePage}/>
+        </Route>
+      </div>
+    </Router>,
+    document.getElementById('mainApp'));
